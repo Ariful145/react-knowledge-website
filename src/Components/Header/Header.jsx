0fff62f40e39e631fe406e1d66f9e45998d132d6
Ariful145 <arif.ahmeed145@gmail.com>
2023-09-14 00:44:40 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import profie from '../../assets/images/profile.png'
+
+const Header = () => {
+   return (
+      <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
+         <h1 className='text-4xl font-bold'>knowledge Cafe</h1>
+         <img src={profie} alt="" />
+      </header>
+   );
+};
+
+export default Header;
